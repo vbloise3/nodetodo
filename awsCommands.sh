34ddf27c30f85fee6d66d83f3a6b6d7810ace47b
Vince Bloise <vbloise3@verizon.net>
2016-05-21 18:44:54 -0400

    Added create tasks table to bash script

--- awsCommands.sh
+++ awsCommands.sh
@@ -4,3 +4,6 @@
 aws dynamodb create-table --table-name todo-user --attribute-definitions AttributeName=uid,AttributeType=S --key-schema AttributeName=uid,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
 ##check status of table create
 aws dynamodb describe-table --table-name todo-user
+
+##create task table
+aws dynamodb create-table --table-name todo-task --attribute-definitions AttributeName=uid,AttributeType=S AttributeName=tid,AttributeType=N --keySchema AttributeName=uid,KeyType=HASH AttributeName=tid,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
