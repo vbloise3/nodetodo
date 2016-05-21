750107d863a00dfffe4c4636e6469c9fe0fd692b
Vince Bloise <vbloise3@verizon.net>
2016-05-21 19:18:42 -0400

    Added secondary index command to bash

--- awsCommands.sh
+++ awsCommands.sh
@@ -7,3 +7,6 @@ aws dynamodb describe-table --table-name todo-user
 
 ##create task table
 aws dynamodb create-table --table-name todo-task --attribute-definitions AttributeName=uid,AttributeType=S AttributeName=tid,AttributeType=N --key-schema AttributeName=uid,KeyType=HASH AttributeName=tid,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+##update todo-task to have a secondary index
+aws dynamodb update-table --table-name todo-task --attribute-definitions AttributeName=uid,AttributeType=S AttributeName=tid,AttributeType=N AttributeName=category,AttributeType=S --global-secondary-index-updates '[{"Create": {"IndexName": "category-index", "KeySchema": [{"AttributeName": "category", "KeyType": "HASH"}, {"AttributeName": "tid", "KeyType": "RANGE"}], "Projection": {"ProjectionType": "ALL"}, "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}}}]'
